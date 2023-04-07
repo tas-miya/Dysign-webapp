9fa5a7b52ef661327f895dbec45779b4d6acf8a6
Iqra Siddiqui <59932139+IqraSiddiqui@users.noreply.github.com>
2023-04-07 07:46:39 +0500

    feature pipeline model integrated

--- DySign/src/App.jsx
+++ DySign/src/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, useLocation} from 'react-router-dom'
 import {Navbar, Hero, Test, Contributors, Home, Result} from './components'
 
 const App = () => {
+  const location = useLocation();
   return (
     <div className='overflow-hidden'>
-      <BrowserRouter>
         <Routes>
           <Route exact path = "/" element={<Home />} />
           <Route path = "/test" element = {<Test />} />
           <Route path = "/result" element={<Result location={location} />} />
         </Routes>
-      </BrowserRouter>
       
       {/* <div className='w-screen h-screen'>
         <Test />
